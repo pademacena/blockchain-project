7a97963fa2290cb5356a6b0dc2d91ecb36163f9f
Paulo Augusto <pademacena@gmail.com>
2022-01-11 22:13:33 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import 'reflect-metadata';
+import 'express-async-errors';
+
+import { router } from './routes';
+
+import { errorHandling } from './middleware/errorHandling';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.use(errorHandling);
+
+export default app;
---
+++ src/services/block.ts
@@ -0,0 +1,37 @@
+import sha256 from 'crypto-js/sha256';
+
+class Block {
+  index: any;
+  previousHash: any;
+  data: any;
+  timestamp: any;
+  difficulty: any;
+  nonce: any;
+  hash: any;
+
+    constructor(index = 0, previousHash = null, data = 'Genesis block', difficulty = 1) {
+        this.index = index; 
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = new Date();
+        this.difficulty = difficulty;
+        this.nonce = 0;
+        
+        this.mine();
+    }
+
+    generateHash() {
+        return sha256(this.index + this.previousHash + JSON.stringify(this.data) + this.timestamp + this.nonce).toString();
+    }
+
+    mine() {
+        this.hash = this.generateHash();
+
+        while (!(/^0*$/.test(this.hash.substring(0, this.difficulty)))) {
+            this.nonce++;
+            this.hash = this.generateHash();
+        }
+    }
+}
+
+export { Block } 
